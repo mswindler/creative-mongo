c8faeb8840bf8abb16ba0595b4ab8f1eab073e89
mswindler <matthew@ionixdesign.com>
2017-11-16 18:01:58 -0700

    Update Comments.js

--- models/Comments.js
+++ models/Comments.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
   title: String,
   upvotes: {type: Number, default: 0},
+  name: String,
 });
 CommentSchema.methods.upvote = function(cb) {
   this.upvotes += 1;
